ba30ce07667dfc5531080d62b38e715164e21bb2
JayC42 <tekloonhue650@gmail.com>
2023-11-13 03:14:44 +0800

    outlayer toggle function added

---
+++ Assets/Scripts/New/HideZoneTrigger.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideZoneTrigger : MonoBehaviour
+{
+    [SerializeField] private GameObject outlayer;
+    void Start()
+    {
+        outlayer.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            outlayer.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            outlayer.SetActive(false);
+        }
+    }
+}
